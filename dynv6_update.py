7cb1f920c80817224f19a52012540ee42d01aa9a
wbc10315558 <skif_wbc@outlook.com>
2023-08-24 09:44:01 +0800

    230824_2 完成 1、修复了在json配置文件中interval,retry_interval字段中输入类数字字符串时报错的bug

--- dynv6_update.py
+++ dynv6_update.py
@@ -14,9 +14,9 @@ def load_configuration_file(filepath: str):
         else:
             raise FileNotFoundError("无法打开 {}：没有那个文件和目录".format(filepath))
     except UnicodeError:
-        exit("无法读取 {}：编码错误（仅支持UTF-8编码）".format(filepath))
+        raise UnicodeError("无法读取 {}：编码错误（仅支持UTF-8编码）".format(filepath))
     except JSONDecodeError:
-        exit("无法读取 {}：配置文件必须符合JSON格式".format(filepath))
+        raise JSONDecodeError("无法读取 {}：配置文件必须符合JSON格式".format(filepath))
     
 def make_url(token, domain, ipv6_address="auto"):
     url = f"http://dynv6.com/api/update?hostname={domain}&ipv6={ipv6_address}&token={token}"
@@ -28,9 +28,9 @@ def main():
         res=send_request(make_url(config["token"],config["domain"]))
         if res.status_code==200:
             print("{} [INFO] Address Updated.".format(asctime()),file=open("dynud.log","r+"))
-            sleep(config["interval"])
+            sleep(int(config["interval"]))
         else:
             print("{} [ERROR] Request Failed: {}".format(asctime(),res.status_code),file=open("dynud.log","r+"))
-            sleep(config["retry_interval"])
+            sleep(int(config["retry_interval"]))
 
 main()
